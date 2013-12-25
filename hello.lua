beb93dd1dcd55bce16a52e743ad11865a1be0bc6
badlyby <badlyby@gmail.com>
2013-12-25 14:52:29 +0800

    add a test

--- hello.lua
+++ hello.lua
@@ -1,5 +1,5 @@
 local usart = require("usart")
-local port = assert(usart.open("/dev/ttyUSB0",115200,8,1,"n"))
+local port = usart.open("/dev/ttyUSB0",115200,8,1,"n")
 port:write("Hello world!")
 usart.msleep(100)
 print(port:read())
